ff138ab3e1860c2e7b915de3005a03be891b88ad
hinagawa <psalimullina@gmail.com>
2021-05-24 14:11:10 +0300

    apolloProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from "@apollo/client";
 import './index.css';
+import client from './const/apollo-client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
